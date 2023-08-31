75032d9cccc587fa437e1f489903a75c9d0d3631
Hasaan Ahmad <hasaanahmad10023@gmail.com>
2023-08-31 13:27:13 +0500

    page update

--- lu-learning/app/[id]/page.jsx
+++ lu-learning/app/[id]/page.jsx
@@ -34,14 +34,17 @@ const page = () => {
                     </div>
                     <div className="p-8">
                         <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{course.heading}</div>
-                        <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{course.heading2}</a>
+                        <div className="uppercase tracking-wide text-xl text-black font-bold font-">{course.heading2}</div>
                         <p className="mt-2 text-gray-500">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut libero ac tortor tristique tristique.
+                            {course.desc}
+                        </p>
+                        <p className="mt-2 text-gray-500">
+                            {course.desc2}
                         </p>
                         <div className="mt-2">
                             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
                                 <FaStar className="inline-block mr-1 text-yellow-500" />
-                                4.8
+                                {course.rating}
                             </span>
                         </div>
                         <div className="mt-2">
@@ -54,6 +57,46 @@ const page = () => {
                             <button className="px-4 py-2 bg-indigo-500 text-white font-semibold rounded-lg hover:bg-indigo-600 focus:outline-none focus:shadow-outline">
                                 Enroll Now
                             </button>
+
+                            <div className="flex items-center justify-between pt-10 lg:pt-4">
+                                <div className="flex gap-2">
+                                    <Image
+                                        src="/assets/banner/check-circle.svg"
+                                        alt="check-image"
+                                        width={30}
+                                        height={30}
+                                        className="smallImage"
+                                    />
+                                    <p className="text-sm sm:text-lg font-normal text-black">
+                                        Recorded And Live Lectures
+                                    </p>
+                                </div>
+                                <div className="flex gap-2">
+                                    <Image
+                                        src="/assets/banner/check-circle.svg"
+                                        alt="check-image"
+                                        width={30}
+                                        height={30}
+                                        className="smallImage"
+                                    />
+                                    <p className="text-sm sm:text-lg font-normal text-black">
+                                        Professional Guidance
+                                    </p>
+                                </div>
+                                <div className="flex gap-2">
+                                    <Image
+                                        src="/assets/banner/check-circle.svg"
+                                        alt="check-image"
+                                        width={30}
+                                        height={30}
+                                        className="smallImage"
+                                    />
+                                    <p className="text-sm sm:text-lg font-normal text-black">
+                                        Certificates
+                                    </p>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
